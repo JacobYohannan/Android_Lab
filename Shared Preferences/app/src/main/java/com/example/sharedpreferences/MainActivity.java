c62461b9c39f2b3266dceb43ea5816ed5dcc6d72
Jacob Yohannan <jacobyohannan191@gmail.com>
2022-08-23 11:58:29 +0530

    shared1

--- Shared Preferences/app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ Shared Preferences/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -14,14 +14,17 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final EditText editText = findViewById(R.id.edit_text);
+        final EditText editText1 = findViewById(R.id.edit_text1);
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String value = editText.getText().toString().trim();
+                String value1 = editText1.getText().toString().trim();
                 SharedPreferences sharedPref = getSharedPreferences("myKey", MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putString("value", value);
+                editor.putString("value1", value1);
                 editor.apply();
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
--- Shared Preferences/app/src/main/java/com/example/sharedpreferences/SecondActivity.java
+++ Shared Preferences/app/src/main/java/com/example/sharedpreferences/SecondActivity.java
@@ -11,8 +11,11 @@ public class SecondActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         TextView textView = findViewById(R.id.text_view);
+        TextView textView1 = findViewById(R.id.text_view1);
         SharedPreferences sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE);
         String value = sharedPreferences.getString("value","");
+        String value1 = sharedPreferences.getString("value1","");
         textView.setText(value);
+        textView1.setText(value1);
     }
 }
