b19e753b610a80e8de9895f1c64630611788dca3
Jacob Yohannan <jacobyohannan191@gmail.com>
2022-08-25 11:01:25 +0530

    ImplicitApp(1)

--- Implicit_Intent/app/src/main/java/com/example/implicit_intent/MainActivity.java
+++ Implicit_Intent/app/src/main/java/com/example/implicit_intent/MainActivity.java
@@ -11,23 +11,39 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    EditText editText;
+    Button b0, b1 ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
+        b0 = findViewById(R.id.b0);
+        b1 = findViewById(R.id.b1);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+        b0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("geo:10.0603, 76.6352"));
-
-                Intent Chooser=intent.createChooser(intent,"launch Map");
+                Intent Chooser=intent.createChooser(intent,"launchMap");
                 startActivity(Chooser);
             }
         });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                String to[]={"jacobyohannan191@gmail.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, to);
+                intent.putExtra(Intent.EXTRA_SUBJECT, "aNdRoId LaB");
+                Intent Chooser=intent.createChooser(intent,"launch Mail");
+                startActivity(Chooser);
+            }
+        });
+
+
     }
     }
