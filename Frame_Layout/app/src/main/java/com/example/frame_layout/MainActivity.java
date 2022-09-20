cb1d8595696bcb62294ab04ebaa5334dea568a72
Jacob Yohannan <jacobyohannan191@gmail.com>
2022-09-20 11:33:23 +0530

    20-09-22

---
+++ Frame_Layout/app/src/main/java/com/example/frame_layout/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.frame_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public  class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView r1,r2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        r1 = (ImageView) findViewById(R.id.image1);
+        r2 = (ImageView) findViewById(R.id.image2);
+        r1.setOnClickListener(this);
+        r2.setOnClickListener(this);
+    }
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.image1) {
+                r1.setVisibility(View.GONE);
+                r2.setVisibility(View.VISIBLE);
+            }
+            else {
+                r1.setVisibility(View.VISIBLE);
+                r2.setVisibility(View.GONE);
+            }
+        }
+
+    }
